cf9a783f4e0f20231edc9d31e3f54cbd1f245384
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-21 06:46:48 +0530

    25.more on strings

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,10 @@ let currentResult = defaultResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = "( " + defaultResult + " + 10) * 3 / 2 - 1";
+let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
 
-outputResult(currentResult, calculationDescription);
+let errorMessage = "An error \n" + "occurred!";
+
+//to see the effect of \n, use white-space:pre css property
+
+outputResult(currentResult, calculationDescription + errorMessage);
