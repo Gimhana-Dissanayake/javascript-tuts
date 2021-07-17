cde1e39f74ea1b00a278e19f2b47bd35c51c433f
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-17 09:13:04 +0530

    81.beyond true/ false: truthy and falsy values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,16 +30,18 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
+  const enteredNumber = getUserNumberInput();
+
   if (
-    calculationType !== "ADD" &&
-    calculationType !== "SUBTRACT" &&
-    calculationType !== "MULTIPLY" &&
-    calculationType !== "DIVIDE"
+    (calculationType !== "ADD" &&
+      calculationType !== "SUBTRACT" &&
+      calculationType !== "MULTIPLY" &&
+      calculationType !== "DIVIDE") ||
+    enteredNumber === 0
   ) {
     return;
   }
 
-  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   let mathOperator;
 
